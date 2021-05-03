6670ec9d7dde034808068e1ab48e708763095276
Marlon Magonjo <39700735+marlonmilesmg@users.noreply.github.com>
2021-05-03 09:39:12 +0200

    layout

--- app/screens/Homepage.js
+++ app/screens/Homepage.js
@@ -14,19 +14,49 @@ function Homepage(props) {
         </View>
 
         <View style={styles.c1}>
-        <Text>CURRENCY 1</Text>
+        <View style={styles.indent}>
+        <Text>IDR</Text>
+        <Text>144 104.50</Text>
+        <Text>( - )</Text>
+        </View>
+        <Text>IDR - Indonesian Rupiah</Text>
+        <Text>1 USD = IDR 14 410.45</Text>
         </View>
 
         <View style={styles.c2}>
-        <Text>CURRENCY 2</Text>
+        <View style={styles.indent}>
+        <Text>EUR</Text>
+        <Text>8.5694</Text>
+        <Text>( - )</Text>
+        </View>
+        <Text>EUR - Euro</Text>
+        <Text>1 USD = 0.8569 </Text>
         </View>
 
         <View style={styles.c3}>
-        <Text>CURRENCY 3</Text>
+        <View style={styles.indent}>
+        <Text>GBP</Text>
+        <Text>7.5894</Text>
+        <Text>( - )</Text>
+        </View>
+        <Text>GBP - British Pound</Text>
+        <Text>1 USD = GBP 0.7589</Text>
         </View>
 
         <View style={styles.c4}>
-        <Text>CURRENCY 4</Text>
+        <View style={styles.indent}>
+        <Text>SGD</Text>
+        <Text>13.6637</Text>
+        <Text>( - )</Text>
+        </View>
+        <Text>SGD - Singapore Dollar</Text>
+        <Text>1 USD = SGD 1.3664</Text>
+        </View>
+
+        <View style={styles.c5}>
+        <View style={styles.indent}>
+        <Text>( + ) Add More Currencies</Text>
+        </View>
         </View>
         </SafeAreaView>
 
@@ -42,7 +72,7 @@ const styles = StyleSheet.create({
         height: 36,
         fontSize: 16,
         backgroundColor: "blue",
-        alignItems: "left", 
+        alignItems: "flex-start", 
     },
     usd : {
         position: "absolute",
@@ -56,19 +86,68 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "space-between",
     },
+    indent : {
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    removeButton : {
+        width: "100%",
+        height: 36,
+        fontWeight: "bold",
+        alignItems: "center",
+        backgroundColor: "green", 
+        flexDirection: "row",
+    },
     c1 : {
         position: "relative",
         top: 50,
         width: "100%",
-        height: 36,
+        height: 100,
         fontWeight: "bold",
         fontSize: 20,
         backgroundColor: "orange",
         alignItems: "baseline", 
-        flexDirection: "row",
+        flexDirection: "column",
         justifyContent: "space-between",
     },
     c2 : {
+        position: "relative",
+        top: 50,
+        width: "100%",
+        height: 100,
+        fontWeight: "bold",
+        fontSize: 20,
+        backgroundColor: "yellow",
+        alignItems: "baseline", 
+        flexDirection: "column",
+        justifyContent: "space-between",
+    },
+    c3 : {
+        position: "relative",
+        top: 50,
+        width: "100%",
+        height: 100,
+        fontWeight: "bold",
+        fontSize: 20,
+        backgroundColor: "green",
+        alignItems: "baseline", 
+        flexDirection: "column",
+        justifyContent: "space-between",
+    },
+    c4 : {
+        position: "relative",
+        top: 50,
+        width: "100%",
+        height: 100,
+        fontWeight: "bold",
+        fontSize: 20,
+        backgroundColor: "red",
+        alignItems: "baseline", 
+        flexDirection: "column",
+        justifyContent: "space-between",
+    },
+    c5 : {
         position: "relative",
         top: 50,
         width: "100%",
@@ -77,32 +156,8 @@ const styles = StyleSheet.create({
         fontSize: 20,
         backgroundColor: "yellow",
         alignItems: "baseline", 
-        flexDirection: "row",
-        justifyContent: "space-between",
-    },
-    c3 : {
-        position: "relative",
-        top: 50,
-        width: "100%",
-        height: 36,
-        fontWeight: "bold",
-        fontSize: 20,
-        backgroundColor: "green",
-        alignItems: "baseline", 
-        flexDirection: "row",
-        justifyContent: "space-between",
-    },
-    c4 : {
-        position: "relative",
-        top: 50,
-        width: "100%",
-        height: 36,
-        fontWeight: "bold",
-        fontSize: 20,
-        backgroundColor: "red",
-        alignItems: "baseline", 
-        flexDirection: "row",
-        justifyContent: "space-between",
+        flexDirection: "column",
+        justifyContent: "center",
     },
 })
 
