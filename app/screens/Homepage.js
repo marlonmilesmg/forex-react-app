78db512824d7faadd17dc310e489798076c86997
Marlon Magonjo <39700735+marlonmilesmg@users.noreply.github.com>
2021-05-03 11:29:39 +0200

    layout complete, TODO API integration

--- app/screens/Homepage.js
+++ app/screens/Homepage.js
@@ -13,49 +13,67 @@ function Homepage(props) {
         <Text>10.0000</Text>
         </View>
 
+        <View style={styles.container}>
         <View style={styles.c1}>
         <View style={styles.indent}>
         <Text>IDR</Text>
         <Text>144 104.50</Text>
-        <Text>( - )</Text>
         </View>
         <Text>IDR - Indonesian Rupiah</Text>
         <Text>1 USD = IDR 14 410.45</Text>
         </View>
+        <View style={styles.removeButton}>
+        <Text>( - )</Text>
+        </View>
+        </View>
 
+        <View style={styles.container}>
         <View style={styles.c2}>
         <View style={styles.indent}>
         <Text>EUR</Text>
         <Text>8.5694</Text>
-        <Text>( - )</Text>
         </View>
         <Text>EUR - Euro</Text>
         <Text>1 USD = 0.8569 </Text>
         </View>
+        <View style={styles.removeButton}>
+        <Text>( - )</Text>
+        </View>
+        </View>
 
+        <View style={styles.container}>
         <View style={styles.c3}>
         <View style={styles.indent}>
         <Text>GBP</Text>
         <Text>7.5894</Text>
-        <Text>( - )</Text>
         </View>
         <Text>GBP - British Pound</Text>
         <Text>1 USD = GBP 0.7589</Text>
         </View>
+        <View style={styles.removeButton}>
+        <Text>( - )</Text>
+        </View>
+        </View>
 
+        <View style={styles.container}>
         <View style={styles.c4}>
         <View style={styles.indent}>
         <Text>SGD</Text>
         <Text>13.6637</Text>
-        <Text>( - )</Text>
         </View>
         <Text>SGD - Singapore Dollar</Text>
         <Text>1 USD = SGD 1.3664</Text>
         </View>
+        <View style={styles.removeButton}>
+        <Text>( - )</Text>
+        </View>
+        </View>
 
-        <View style={styles.c5}>
+        <View style={styles.container}>
+        <View style={styles.addCurrency}>
         <View style={styles.indent}>
         <Text>( + ) Add More Currencies</Text>
+        </View>
         </View>
         </View>
         </SafeAreaView>
@@ -70,8 +88,9 @@ const styles = StyleSheet.create({
         top: 20,
         width: "100%",
         height: 36,
+        padding: 8,
         fontSize: 16,
-        backgroundColor: "blue",
+        backgroundColor: "white",
         alignItems: "flex-start", 
     },
     usd : {
@@ -79,12 +98,18 @@ const styles = StyleSheet.create({
         top: 50,
         width: "100%",
         height: 36,
+        padding: 8,
         fontWeight: "bold",
         fontSize: 20,
-        backgroundColor: "red",
+        backgroundColor: "yellow",
         alignItems: "baseline", 
         flexDirection: "row",
         justifyContent: "space-between",
+    },
+    container : {
+        width: "100%",
+        flexDirection: "row",
+        padding: 10,
     },
     indent : {
         width: "100%",
@@ -92,18 +117,23 @@ const styles = StyleSheet.create({
         justifyContent: "space-between",
     },
     removeButton : {
-        width: "100%",
-        height: 36,
-        fontWeight: "bold",
-        alignItems: "center",
-        backgroundColor: "green", 
-        flexDirection: "row",
+        top: 50,
+        width: "20%",
+        height: 100,
+        marginTop: 15,
+        padding: 25,
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        backgroundColor: "red", 
     },
     c1 : {
         position: "relative",
         top: 50,
-        width: "100%",
+        width: "80%",
         height: 100,
+        marginTop: 15,
+        padding: 10,
         fontWeight: "bold",
         fontSize: 20,
         backgroundColor: "orange",
@@ -114,8 +144,10 @@ const styles = StyleSheet.create({
     c2 : {
         position: "relative",
         top: 50,
-        width: "100%",
+        width: "80%",
         height: 100,
+        marginTop: 15,
+        padding: 10,
         fontWeight: "bold",
         fontSize: 20,
         backgroundColor: "yellow",
@@ -126,11 +158,13 @@ const styles = StyleSheet.create({
     c3 : {
         position: "relative",
         top: 50,
-        width: "100%",
+        width: "80%",
         height: 100,
+        marginTop: 15,
+        padding: 10,
         fontWeight: "bold",
         fontSize: 20,
-        backgroundColor: "green",
+        backgroundColor: "orange",
         alignItems: "baseline", 
         flexDirection: "column",
         justifyContent: "space-between",
@@ -138,24 +172,26 @@ const styles = StyleSheet.create({
     c4 : {
         position: "relative",
         top: 50,
-        width: "100%",
+        width: "80%",
         height: 100,
-        fontWeight: "bold",
-        fontSize: 20,
-        backgroundColor: "red",
-        alignItems: "baseline", 
-        flexDirection: "column",
-        justifyContent: "space-between",
-    },
-    c5 : {
-        position: "relative",
-        top: 50,
-        width: "100%",
-        height: 36,
+        marginTop: 15,
+        padding: 10,
         fontWeight: "bold",
         fontSize: 20,
         backgroundColor: "yellow",
         alignItems: "baseline", 
+        flexDirection: "column",
+        justifyContent: "space-between",
+    },
+    addCurrency : {
+        position: "relative",
+        top: 50,
+        width: "100%",
+        height: 36,
+        marginTop: 15,
+        fontWeight: "bold",
+        fontSize: 20,
+        backgroundColor: "green", 
         flexDirection: "column",
         justifyContent: "center",
     },
